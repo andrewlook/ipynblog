615e2f330bd2b47da821f789af1936b6ee2f7556
Andrew Look <andrew.m.look@gmail.com>
2018-04-04 19:00:36 -0700

    v0.1.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.0',  # Required
+    version='0.1.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -150,6 +150,8 @@ setup(
         'dev': [
             # TODO(look) fill these in
             'check-manifest',
+            'twine',
+            'wheel',
         ],
         'test': [
             'coverage',
