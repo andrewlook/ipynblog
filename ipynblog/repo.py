b7bd76a1fde11da66c815c1b77c0bf7cf683e236
Andrew Look <andrew.m.look@gmail.com>
2018-04-07 19:25:15 -0700

    starting gitclonedepth1 approach

---
+++ ipynblog/repo.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import subprocess
+import io
+import yaml
+import os
+
+
+def call(x):
+    subprocess.check_call(x.split())
+
+
+def git_clone(url, project_root):
+    call('git clone --depth 1 {url} {name}'.format(url=url, name=project_root))
+    call('rm -rf {name}/.git/'.format(name=project_root))
+
+
+def find_yaml_config(project_root):
+    yaml_fname = os.path.join(project_root, 'ipynblog.yaml')
+    if not os.path.isfile(yaml_fname):
+        raise ValueError('{yaml_fname} not found from template at {url}'
+                         .format(yaml_fname=yaml_fname))
+    with io.open(yaml_fname) as yaml_fd:
+        ipynblog_config = yaml.load(yaml_fd)
+    return ipynblog_config
+
+
+def git_init():
+    call('git init')
+    call('git add -f .')
+    call('git commit -m init')
+
